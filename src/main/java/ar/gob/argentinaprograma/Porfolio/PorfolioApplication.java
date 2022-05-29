88fa7ce590195bbcd9d0ca36ed419a5fffe769e5
Juan Nebbia <juan.nebbia@gmail.com>
2022-05-29 15:08:26 -0300

    cors de nuevo

--- src/main/java/ar/gob/argentinaprograma/Porfolio/PorfolioApplication.java
+++ src/main/java/ar/gob/argentinaprograma/Porfolio/PorfolioApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PorfolioApplication {
@@ -15,11 +16,15 @@ public class PorfolioApplication {
 		SpringApplication.run(PorfolioApplication.class, args);
 	}
         
+        
+        
+        
+        
         @Bean
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://porfolio-web-ap.herokuapp.com/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
