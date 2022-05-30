c635b955005714b244c1b2ac127106fffcee522e
Juan Nebbia <juan.nebbia@gmail.com>
2022-05-29 22:13:57 -0300

    vuelta atrás

--- src/main/java/ar/gob/argentinaprograma/Porfolio/PorfolioApplication.java
+++ src/main/java/ar/gob/argentinaprograma/Porfolio/PorfolioApplication.java
@@ -3,13 +3,12 @@ package ar.gob.argentinaprograma.Porfolio;
 import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class PorfolioApplication {
 
 	public static void main(String[] args) {
