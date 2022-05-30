6bb4b72c27581a064aa459e297e0197df81c37ee
Juan Nebbia <juan.nebbia@gmail.com>
2022-05-29 21:59:37 -0300

    sin bean

--- src/main/java/ar/gob/argentinaprograma/Porfolio/controller/EducacionController.java
+++ src/main/java/ar/gob/argentinaprograma/Porfolio/controller/EducacionController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "*")
 public class EducacionController {
     private final EducacionService educacionService;
 
