3e71f74052cf1448cc8d7cf787d1da38bd603539
Juan Nebbia <juan.nebbia@gmail.com>
2022-10-10 12:38:32 -0300

    cors final

--- src/main/java/ar/gob/argentinaprograma/Porfolio/PorfolioApplication.java
+++ src/main/java/ar/gob/argentinaprograma/Porfolio/PorfolioApplication.java
@@ -20,7 +20,7 @@ public class PorfolioApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://juannebbia.com.ar/"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://juannebbia.com.ar/", "https://porfolioweb-cfeef.web.app/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
